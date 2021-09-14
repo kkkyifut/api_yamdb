366d2a775b24b767b34e7848360b50c9b8296a85
Юрий Яковлев <ura.yakovlev@yandex.ru>
2021-09-14 12:06:42 +0300

    Правки по ревью. Поправил валидатор, докстринги и модели

--- api_yamdb/reviews/models.py
+++ api_yamdb/reviews/models.py
@@ -1,16 +1,8 @@
-import datetime
-
-from django.core.exceptions import ValidationError
+from django.core.validators import MaxValueValidator, MinValueValidator
 from django.db import models
 
 from users.models import User
-
-SCORE_CHOICES = [(r, r) for r in range(1, 11)]
-
-
-def validate_year(value):
-    if value < 0 and value > datetime.date.today().year:
-        raise ValidationError(f'0 < year <= {datetime.date.today().year}')
+from .validators import validate_year
 
 
 class Category(models.Model):
@@ -69,22 +61,22 @@ class TitleGenre(models.Model):
 
 
 class Review(models.Model):
-    text = models.TextField(verbose_name='Текст')
+    text = models.TextField(verbose_name='Отзыв')
     author = models.ForeignKey(
         User, related_name='reviews', on_delete=models.CASCADE,
     )
     score = models.PositiveSmallIntegerField(
-        'Оценка', default=1, blank=False, null=False, choices=SCORE_CHOICES
+        'Оценка', default=1, blank=False, null=False,
+        validators=[MinValueValidator(1), MaxValueValidator(10)]
     )
     pub_date = models.DateTimeField('Дата публикации', auto_now_add=True)
     title = models.ForeignKey(
         Title, related_name='reviews', on_delete=models.CASCADE
     )
 
-    def __str__(self):
-        return(f'{self.id}. {self.text[:50]}')
-
     class Meta:
+        verbose_name = 'Отзыв'
+        verbose_name_plural = 'Отзывы'
         ordering = ['-pub_date']
         constraints = [
             models.UniqueConstraint(
@@ -93,9 +85,12 @@ class Review(models.Model):
             )
         ]
 
+    def __str__(self):
+        return(f'{self.id}. {self.text[:50]}')
+
 
 class Comment(models.Model):
-    text = models.TextField(verbose_name='Текст')
+    text = models.TextField(verbose_name='Комментарий')
     author = models.ForeignKey(
         User, on_delete=models.CASCADE, related_name='comments'
     )
@@ -106,8 +101,10 @@ class Comment(models.Model):
         Review, on_delete=models.CASCADE, related_name='comments'
     )
 
+    class Meta:
+        verbose_name = 'Комментарий'
+        verbose_name_plural = 'Комментарии'
+        ordering = ['-pub_date']
+
     def __str__(self):
         return self.text[:50]
-
-    class Meta:
-        ordering = ['-pub_date']
