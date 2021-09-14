fcdebde8abd0a8f949a724cd70ab9a0f6da3bef5
Юрий Яковлев <ura.yakovlev@yandex.ru>
2021-09-14 12:08:13 +0300

    Мелкая правка

--- api_yamdb/reviews/validators.py
+++ api_yamdb/reviews/validators.py
@@ -4,4 +4,4 @@ from django.utils import timezone
 
 def validate_year(value):
     if value > timezone.now().year:
-        raise ValidationError(f'0 < year <= {timezone.now().year}')
+        raise ValidationError(f'year <= {timezone.now().year}')
