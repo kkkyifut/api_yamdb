f6c6ed4bd5ec44bae58e35737241105292fba292
Егор Коростелев <egor.fipu@gmail.com>
2021-09-14 11:37:54 +0700

    remind_confirmation_code

--- api_yamdb/users/utilities.py
+++ api_yamdb/users/utilities.py
@@ -14,3 +14,18 @@ def send_confirmation_code(user):
         context
     )
     user.email_user(subject, body_text)
+
+
+def remind_confirmation_code(user):
+    context = {
+        'user': user,
+    }
+    subject = render_to_string(
+        'emails/remind_confirmation_code_subject.txt',
+        context
+    )
+    body_text = render_to_string(
+        'emails/remind_confirmation_code_body.txt',
+        context
+    )
+    user.email_user(subject, body_text)
